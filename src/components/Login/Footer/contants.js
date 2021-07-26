81b54be5f798a81555362ae57c869a50318a55d1
David Cingolani <cingolanidavid@gmail.com>
2021-07-26 19:55:07 -0300

    improving namese

--- src/components/Login/Footer/contants.js
+++ src/components/Login/Footer/contants.js
@@ -10,14 +10,14 @@ export const hashtagsLink = '';
 export const locationsLink = '';
 
 export const linksItems = [
-    { text: 'Information', link: 'https://about.instagram.com/' },
+    { text: 'About', link: 'https://about.instagram.com/' },
     { text: 'Blog', link: 'https://about.instagram.com/blog' },
-    { text: 'Job', link: 'https://about.instagram.com/about-us/careers' },
+    { text: 'Jobs', link: 'https://about.instagram.com/about-us/careers' },
     { text: 'Help', link: 'https://help.instagram.com/' },
     { text: 'API', link: 'https://developers.facebook.com/docs/instagram' },
     { text: 'Privacy', link: 'https://help.instagram.com/519522125107875' },
     { text: 'Terms', link: 'https://help.instagram.com/581066165581870' },
-    { text: 'Featured accounts', link: 'https://www.instagram.com/directory/profiles/' },
+    { text: 'Top Accounts', link: 'https://www.instagram.com/directory/profiles/' },
     { text: 'Hashtags', link: 'https://www.instagram.com/directory/hashtags/' },
     { text: 'Locations', link: 'https://www.instagram.com/explore/locations/' }
 ]
